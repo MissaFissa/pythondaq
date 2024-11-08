924da66322ce5176c0e5d014e119ef96c37868b3
MissaFissa <meho11@outlook.com>
2024-11-08 14:55:43 +0100

    safety commit

--- arduino_device.py
+++ arduino_device.py
@@ -1,28 +1,55 @@
 import pyvisa
 
 def list_resources():
+    """Get list of available ports.
 
+    Returns:
+        list: list of all availabe ports.
+    """
     rm = pyvisa.ResourceManager("@py")
 
     return rm.list_resources()
 
 class ArduinoVISADevice:
-
+    """Communicate with Arduino.
+    """
     def __init__(self, port):
+        """Ask port from user.
 
+        Args:
+            port (str): used port.
+        """
         rm = pyvisa.ResourceManager("@py")
         self.device = rm.open_resource(port, read_termination = "\r\n", write_termination = "\n")
 
     def get_identification(self):
+        """return identification string of connected device.
 
+        Returns:
+            str: identification string of connected device.
+        """
         return self.device.query("*IDN?")
 
     def set_output_value(self, value):
+        """Set value on the output channel.
 
-        return self.device.query("OUT:CH0 "+str(value)+"")
+        Args:
+            value (int): value for the output channel.
+
+        Returns:
+            _type_: _description_
+        """
+        self.device.query("OUT:CH0 "+str(value)+"")
 
     def get_input_value(self, channel):
+        """_summary_
 
+        Args:
+            channel (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
         return self.device.query("MEAS:CH"+str(channel)+"?")
 
     def get_input_voltage(self, channel):
@@ -32,3 +59,6 @@ class ArduinoVISADevice:
     def get_output_value(self):  
 
         return self.device.query("OUT:CH0?")
+
+if __name__ == "__main__":
+    help(ArduinoVISADevice)
--- view.py
+++ view.py
@@ -4,15 +4,14 @@ from diode_experiment import DiodeExperiment
 
 experiment = DiodeExperiment()
 
-voltages_LED, currents_LED, std_voltages, std_currents, mean_voltages, mean_currents = experiment.scan(start = 0, stop = 1023, iterations = 5)
+voltages_LED, currents_LED, std_voltages, std_currents, mean_voltages, mean_currents = experiment.scan(start = 0, stop = 1023, iterations = 2)
 csv_file = experiment.csv_file()
 
 print(mean_voltages[0:10])
 
 fig1 = plt.figure(figsize = (10, 8))
 
-plt.errorbar(mean_voltages, mean_currents, xerr = std_voltages, yerr = std_currents, fmt = 'bo', markersize = 1.5, elinewidth = 0.5, ecolor = 'r')
-
+plt.errorbar(mean_voltages, mean_currents, xerr = std_voltages, yerr = std_currents, fmt = 'bo', markersize = 1.5, elinewidth = 0.5, ecolor = 'g')
 plt.xticks(rotation = 'vertical')
 plt.xlabel('Voltages LED [V]')
 plt.ylabel('Currents LED [A]')
