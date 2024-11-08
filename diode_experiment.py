621a4ee727a13927ba864019ba71598cd0ea8ee7
MissaFissa <meho11@outlook.com>
2024-11-08 13:48:40 +0100

    safety

--- diode_experiment.py
+++ diode_experiment.py
@@ -8,9 +8,6 @@ class DiodeExperiment:
 
         self.device = ArduinoVISADevice(port = "ASRL9::INSTR")
         self.resistance_resistor = 220
-        self.voltages_resistor = []
-        self.voltages_LED = []
-        self.currents_LED = []
         self.voltages_LED_repeats = []
         self.currents_LED_repeats = []
         self.standard_deviations_voltages = []
@@ -24,32 +21,38 @@ class DiodeExperiment:
 
         for _ in range(iterations):
 
+            voltages_resistor = []
+            voltages_LED = []
+            currents_LED = []
+
             for raw_value in raw_values:
 
                 self.device.set_output_value(value = raw_value)
                 voltage_resistor = self.device.get_input_voltage(channel = 2)
                 voltage_LED = self.device.get_input_voltage(channel = 1) - voltage_resistor
-                self.voltages_resistor.append(voltage_resistor)
-                self.voltages_LED.append(voltage_LED)
+                voltages_resistor.append(voltage_resistor)
+                voltages_LED.append(voltage_LED)
                 current_LED = voltage_resistor / self.resistance_resistor
-                self.currents_LED.append(current_LED)
+                currents_LED.append(current_LED)
             
-            self.voltages_LED_repeats.append(self.voltages_LED)
-            self.currents_LED_repeats.append(self.currents_LED)
+            self.voltages_LED_repeats.append(voltages_LED)
+            self.currents_LED_repeats.append(currents_LED)
 
-        self.standard_deviations_voltages = np.std((np.array(self.voltages_LED_repeats)), axis = 0)
-        self.standard_deviations_currents = np.std((np.array(self.currents_LED_repeats)), axis = 0)
+        self.standard_deviations_voltages = np.std((np.array(self.voltages_LED_repeats)), axis = 0) / np.sqrt(iterations)
+        self.standard_deviations_currents = np.std((np.array(self.currents_LED_repeats)), axis = 0) / np.sqrt(iterations)
         self.means_voltages = np.mean((np.array(self.voltages_LED_repeats)), axis = 0)
         self.means_currents = np.mean((np.array(self.currents_LED_repeats)), axis = 0)
 
-        return self.voltages_LED, self.currents_LED, self.standard_deviations_voltages, self.standard_deviations_currents, self.means_voltages, self.means_currents
+        return voltages_LED, currents_LED, self.standard_deviations_voltages, self.standard_deviations_currents, self.means_voltages, self.means_currents
 
-# with open('pythondaq/metingen.csv', 'w', newline = '') as csvfile:
+    def csv_file(self):
 
-#     writer = csv.writer(csvfile)
-#     header = ['Voltages LED', 'Currents LED']
-#     writer.writerow(header)
+        with open('pythondaq/metingen.csv', 'w', newline = '') as csvfile:
 
-#     for voltage_LED, current_LED in zip(voltages_LED, currents_LED):
+            writer = csv.writer(csvfile)
+            header = ['mean Voltages LED', 'mean Currents LED']
+            writer.writerow(header)
 
-#         writer.writerow([voltage_LED, current_LED])
+            for mean_voltage_LED, mean_current_LED in zip(self.means_voltages, self.means_currents):
+
+                writer.writerow([mean_voltage_LED, mean_current_LED])
--- view.py
+++ view.py
@@ -5,10 +5,13 @@ from diode_experiment import DiodeExperiment
 experiment = DiodeExperiment()
 
 voltages_LED, currents_LED, std_voltages, std_currents, mean_voltages, mean_currents = experiment.scan(start = 0, stop = 1023, iterations = 5)
+csv_file = experiment.csv_file()
 
-fig = plt.figure(figsize = (10, 8))
+print(mean_voltages[0:10])
 
-plt.errorbar(mean_voltages, mean_currents, std_voltages, std_currents)
+fig1 = plt.figure(figsize = (10, 8))
+
+plt.errorbar(mean_voltages, mean_currents, xerr = std_voltages, yerr = std_currents, fmt = 'bo', markersize = 1.5, elinewidth = 0.5, ecolor = 'r')
 
 plt.xticks(rotation = 'vertical')
 plt.xlabel('Voltages LED [V]')
