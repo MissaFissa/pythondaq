897c5f4c060b5200cedcbd0d140caf7a1fd66e5d
MissaFissa <meho11@outlook.com>
2024-11-12 12:17:00 +0100

    safety

--- src/pythondaq/view.py
+++ src/pythondaq/view.py
@@ -5,11 +5,25 @@ import csv
 from pythondaq.diode_experiment import list_resources
 from pythondaq.diode_experiment import DiodeExperiment
 
-experiment = DiodeExperiment()
+def initiate_experiment():
+    
+    experiment = DiodeExperiment()
 
-errors_voltages, errors_currents, means_voltages, means_currents = experiment.scan(start = 0, stop = 1023, iterations = 1)
+    errors_voltages, errors_currents, means_voltages, means_currents = experiment.scan(start = 0, stop = 1023, iterations = 2)
 
-def create_csv_file():
+    csv_file = create_csv_file(means_voltages, means_currents)
+
+    fig = plt.figure(figsize = (10, 8))
+
+    plt.errorbar(means_voltages, means_currents, xerr = errors_voltages, yerr = errors_currents, fmt = 'bo', markersize = 1.25, elinewidth = 0.5, capsize = 1, ecolor = 'r')
+    plt.xticks(rotation = 'vertical')
+    plt.xlabel('Voltage LED [V]')
+    plt.ylabel('Current LED [A]')
+    plt.tight_layout()
+
+    plt.show()
+
+def create_csv_file(means_voltages, means_currents):
     """Create a csv file containing the means of the voltages and the means of the currents.
     """
     with open('metingen.csv', 'w', newline = '') as csvfile:
@@ -21,15 +35,3 @@ def create_csv_file():
         for mean_voltage_LED, mean_current_LED in zip(means_voltages, means_currents):
 
             writer.writerow([mean_voltage_LED, mean_current_LED])
-            
-csv_file = create_csv_file()
-
-fig = plt.figure(figsize = (10, 8))
-
-plt.errorbar(means_voltages, means_currents, xerr = errors_voltages, yerr = errors_currents, fmt = 'bo', markersize = 1.25, elinewidth = 0.5, capsize = 1, ecolor = 'r')
-plt.xticks(rotation = 'vertical')
-plt.xlabel('Voltage LED [V]')
-plt.ylabel('Current LED [A]')
-plt.tight_layout()
-
-plt.show()
