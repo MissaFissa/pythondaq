080cfc2023de834c3a412e2592c7439bd5b3dc6c
MissaFissa <meho11@outlook.com>
2024-11-19 12:42:38 +0100

    final commit for today's class
    Done with cli, just going to add some updates and slight adjustments/improvements in the coming days.

--- src/pythondaq/cli.py
+++ src/pythondaq/cli.py
@@ -1,4 +1,7 @@
+"""Command line interface for the experiment.
+"""
 import click
+import matplotlib.pyplot as plt
 import csv
 from pythondaq.diode_experiment import list_resources
 from pythondaq.diode_experiment import DiodeExperiment
@@ -11,42 +14,60 @@ def diode():
 
 @diode.command()
 def list():
-
+    """Print list of connected devices.
+    """
     print(list_resources())
 
 @diode.command()
 @click.argument("device")
 def info(device):
-
+    """Print identification string of device.\n
+    DEVICE: portname of device.
+    """
     experiment = DiodeExperiment(device)
     print(experiment.get_identification())
 
 @diode.command()
+@click.argument("device")
 @click.argument("start")
 @click.argument("stop")
-@click.option("-r", "--repeats", default = 1)
-@click.option("-o", "--output", default = "")
-def scan(start, stop, repeats, output):
-
-    experiment = DiodeExperiment()
+@click.option("-r", "--repeats", default = 1, help = "Select the amount of repetitions of the experiment.")
+@click.option("-o", "--output", default = "", help = "Choose name of CSV file if wanted, otherwise just ignore it.")
+@click.option("--graph/--no-graph", default = False, help = "Choose whether or not to show a graph.")
+def scan(start, stop, repeats, output, device, graph):
+    """Initiate experiment.\n
+    DEVICE: portname of device, START: minimum value of the voltage range, STOP: maximum value of the voltage range.
+    """
+    experiment = DiodeExperiment(device)
 
     start_DAC = int(float(start) * (1023 / 3.3))
     stop_DAC = int(float(stop) * (1023 / 3.3))
 
-    voltages_LED, currents_LED = experiment.scan(start = start_DAC, stop = stop_DAC, iterations = repeats)[4:6]
-
+    errors_voltages, errors_currents, means_voltages, means_currents, voltages_LED, currents_LED = experiment.scan(start = start_DAC, stop = stop_DAC, iterations = repeats)
 
     if output != "":
 
         with open(f'{output}.csv', 'w', newline = '') as csvfile:
 
             writer = csv.writer(csvfile)
-            header = ['Voltages LED', 'Currents LED']
+            header = ['Mean voltages LED', 'Mean currents LED', 'Errors voltages', 'Errors currents']
             writer.writerow(header)
-            
-            for voltages_LED, currents_LED in zip(voltages_LED[0], currents_LED[0]):
 
-                writer.writerow([voltages_LED, currents_LED])
+            for mean_voltage_LED, mean_current_LED, errors_voltages, errors_currents in zip(means_voltages, means_currents, errors_voltages, errors_currents):
+
+                writer.writerow([mean_voltage_LED, mean_current_LED, errors_voltages, errors_currents])
+
+    if graph:
+
+        plt.figure(figsize = (10, 8))
+
+        plt.errorbar(means_voltages, means_currents, xerr = errors_voltages, yerr = errors_currents, fmt = 'bo', markersize = 1.25, elinewidth = 0.5, capsize = 1, ecolor = 'r')
+        plt.xticks(rotation = 'vertical')
+        plt.xlabel('Mean voltages LED [V]')
+        plt.ylabel('Mean currents LED [A]')
+        plt.tight_layout()
+
+        plt.show()
 
 if __name__ == "__main__":
 
